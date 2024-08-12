53fed69064b422b49805a13cea0bcf4bab26cd6a
nithinelluru12 <nithinelluru12345@gmail.com>
2024-08-12 15:00:51 +0530

    examples for special numbers

--- assignment.js
+++ assignment.js
@@ -4,3 +4,10 @@ console.log(3.14)
 console.log(-150)
 console.log(0XFF)
 console.log(10/3)
+
+// special numbers
+console.log(1000/0)
+console.log(-1000/0)
+console.log("hello"/2)
+console.log(22/0)
+console.log(0/0)
