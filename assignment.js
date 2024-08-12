c88af67dc865d7394c6662d78b77e559d2949945
nithinelluru12 <nithinelluru12345@gmail.com>
2024-08-12 15:11:31 +0530

    written exaples for special number

--- assignment.js
+++ assignment.js
@@ -6,8 +6,24 @@ console.log(0XFF)
 console.log(10/3)
 
 // special numbers
+    // infinity
 console.log(1000/0)
+console.log(1e1000)
+console.log(Infinity * 2)
+console.log(Math.pow(10, 308) * 10)
+console.log(Infinity + 100)
+    // -infinity
+console.log("------------------------------")
 console.log(-1000/0)
-console.log("hello"/2)
-console.log(22/0)
+console.log(-1e1000)
+console.log(-Infinity * 2)
+console.log(-Math.pow(10, 308) * 10)
+console.log(-Infinity + 100)
+
+    // NaN
+console.log("------------------------------")
 console.log(0/0)
+console.log("hello"*3)
+console.log(21-"hello")
+console.log(0*Infinity)
+console.log("hello"/3)
