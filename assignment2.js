e18437e0e4173f948f861b64c1a24c19b7ab5459
ellurunithin <nithinelluru12345@msitprogram.net>
2024-08-13 21:58:49 +0530

    differ between null and undefined

---
+++ assignment2.js
@@ -0,0 +1,10 @@
+// Q1. [1 Point] Explain the difference between null and undefined in JavaScript.
+
+
+// undefined is what you get when you create a variable but don’t give it a value.
+// ex:  let x;
+// console.log(x);
+
+// null is used when you want to show that a variable has no value on purpose.
+// ex: let obj = { name: "John", age: null };
+// console.log(obj.age);
