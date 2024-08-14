a69131a6023596df0cfd0cda79a0d37c8c80f395
ellurunithin <nithinelluru12345@msitprogram.net>
2024-08-14 22:59:55 +0530

    history of javascript

---
+++ assignment3.js
@@ -0,0 +1,16 @@
+//1. History of JavaScript
+// In the early days of the internet, Microsoft offered a free web browser, 
+// which attracted users because Netscape's browser was not free. In 1990, Mosaic was a popular browser,
+//  but it also wasn't free. The creators of Mosaic later developed Netscape, which was renamed Netscape Navigator.
+
+// JavaScript was created in 1995 by Brendan Eich at Netscape to compete with Microsoft.
+//  It was first called Mocha, then LiveScript, and finally JavaScript. 
+// Microsoft made a similar version called JScript.
+
+// In 1997, JavaScript became a standard through the European Computer Manufacturers Association (ECMA). 
+// Since then, JavaScript has been improved with new features, following ECMAScript standards, 
+// and it gets updates every year since 2016. JavaScript is very important for web development, 
+// used both on the front end and back end (with Node.js), and it works on both mobile and desktop applications.
+
+
+
