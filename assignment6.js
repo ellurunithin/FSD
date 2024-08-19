a7676dd0817bdc504e0f54084b18418350213429
ellurunithin <nithinelluru12345@msitprogram.net>
2024-08-19 23:42:06 +0530

    wrote the arrow and constructor function

--- assignment6.js
+++ assignment6.js
@@ -36,3 +36,52 @@ console.log(countBs("BOB"));
 // → 2
 console.log(countChar("kakkerlak", "k")); 
 // → 4
+
+
+// extra questions
+// Q1.
+// Define the function min that takes two arguments and returns their minimum. We should not
+// use Math.min() we need to implement one our own.
+
+// console.log(min(0, 10));
+// // → 0
+// console.log(min(0, -10));
+// // → -10
+
+
+// using the constructor function
+const min = new Function('a', 'b', 'return a < b ? a : b;');
+
+console.log(min(0, 10)); // → 0
+console.log(min(0, -10)); // → -10
+
+// We’ve seen that we can use % (the remainder operator) to test whether a number is even or
+// odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a
+// positive whole number is even or odd:
+// Zero is even.
+// One is odd.
+// For any other number N, its evenness is the same as N - 2.
+// Define a recursive function isEven corresponding to this description. The function should
+// accept a single parameter (a positive, whole number) and return a Boolean.
+// Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+
+// console.log(isEven(50));
+// // → true
+// console.log(isEven(75));
+// // → false
+// console.log(isEven(-1));
+// // → ??
+
+// Arrow funcitons
+
+const isEven = (n) => {
+    if (n < 0) n = -n; // Handle negative numbers
+    if (n === 0) return true;
+    if (n === 1) return false;
+    return isEven(n - 2);
+  };
+  
+  console.log(isEven(50)); // → true
+  console.log(isEven(75)); // → false
+  console.log(isEven(-1)); // → false
+  
