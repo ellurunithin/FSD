47d7745caea97bb15d2aad977a7a29687c63dc9e
ellurunithin <nithinelluru12345@msitprogram.net>
2024-08-22 17:21:54 +0530

    wrote the map() and example

---
+++ assignment9.js
@@ -0,0 +1,12 @@
+// Q1. Explain the .map() function in 
+// JavaScript and provide three examples with detailed explanations.
+// The .map() function in JavaScript is like a conveyor belt in a factory. 
+// Imagine you have a row of objects, and you want to transform each one into something new.
+//  You put each object on the conveyor belt, and as it moves along, 
+//  a machine (the .map() function) modifies it. 
+//  The result is a new row of objects that have been changed in some way.
+
+// Example: Doubling Numbers
+// Imagine you have a list of numbers [1, 2, 3, 4], and you want to double each number. 
+// Using .map(), you get a new list [2, 4, 6, 8]. The original list stays the same; 
+// the new list has the doubled values.
