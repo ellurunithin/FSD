e02f2c1ecd3e52863813f9eba5a2ba88b93c1adc
ellurunithin <nithinelluru12345@msitprogram.net>
2024-08-22 17:34:04 +0530

    wrote the reduce method and example

--- assignment9.js
+++ assignment9.js
@@ -10,3 +10,13 @@
 // Imagine you have a list of numbers [1, 2, 3, 4], and you want to double each number. 
 // Using .map(), you get a new list [2, 4, 6, 8]. The original list stays the same; 
 // the new list has the doubled values.
+
+
+// Q2. Explain the .reduce() function in JavaScript and 
+// provide three examples with detailed explanations.
+// The .reduce() function is like a snowball rolling down a hill, gathering more snow as it goes. 
+// It starts with an initial value (the snowball), then adds to it with each step until it reaches the bottom of the hill. 
+// This function is great for turning a list of items into a single result.
+// Example : Adding Numbers Together
+// If you have a list of numbers [1, 2, 3, 4], and you want to add them all together, 
+// .reduce() will give you 10 as the result.
